d563d7dbf4a74a3dbebfb72de07a8c6fd308eda3
Colin Brophy <colin@brophys.com>
2011-09-05 14:23:34 +0100

    Moved headers to 'include' dir
    Signed-off-by: Colin Brophy <colin@brophys.com>

--- include/utils.h
+++ include/utils.h
@@ -1,10 +1,14 @@
+#ifndef UTILS_H
+#define UTILS_H
 #include "consts.h"
 #include <stdlib.h>
 #include <stdio.h>
 
-const int char_error = EOF;
+#define char_error EOF
 
 void error_term(const char* error_msg, ...);
 void* safe_malloc(size_t size);
 uint char_to_uint(char c);
 int uint_to_char(uint n);
+
+#endif /* UTILS_H */
